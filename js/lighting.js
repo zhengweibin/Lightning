c138ed63272238954aa702447b01944319b24ec4
zhengweibin <wodebinghe@163.com>
2016-07-05 09:13:18 +0800

    roll back for the lightning files

--- js/lighting.js
+++ js/lighting.js
@@ -1,10 +1,6 @@
 (function($){
     $.fn.Lightning = function(content,type) {
         this.html(content);
-		if(type === 'add'){
-			this.css("background-color","#3DC1FA")			
-		}else{
         this.css("background-color","#ffb2b0");			
-		}
     };
 })(jQuery);
